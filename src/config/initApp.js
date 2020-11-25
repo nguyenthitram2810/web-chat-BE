663d382757dca18a4e4020e777050464f0c96710
nguyenthitram2810 <nguyenthitram2010a@gmail.com>
2020-11-25 21:21:13 +0700

    hey

--- src/config/initApp.js
+++ src/config/initApp.js
@@ -10,12 +10,12 @@ import configSocket from '../config/socket';
 
 export default (app) => {
     app.use((req, res, next) => {
-      const allowedOrigins = ['*'];
-      const origin = req.headers.origin;
-      if (allowedOrigins.includes(origin)) {
-           res.setHeader('Access-Control-Allow-Origin', origin);
-      }
-
+    //   const allowedOrigins = ['*'];
+    //   const origin = req.headers.origin;
+    //   if (allowedOrigins.includes(origin)) {
+    //        res.setHeader('Access-Control-Allow-Origin', origin);
+    //   }
+      res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
       res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
       res.header('Access-Control-Allow-Credentials', true);
