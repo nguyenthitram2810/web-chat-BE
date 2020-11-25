97b3d30236d00e0b3a547eebc52b502c731f1b84
nguyenthitram2810 <nguyenthitram2010a@gmail.com>
2020-11-25 23:11:29 +0700

    fix bug

--- src/config/initApp.js
+++ src/config/initApp.js
@@ -9,20 +9,19 @@ import Model from '../database/models/index';
 import configSocket from '../config/socket';
 
 export default (app) => {
-    // app.use((req, res, next) => {
-    //     res.header('Access-Control-Allow-Origin', '*');
-    //     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    //     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
-    //     next();
-    // });
-    app.use(cors({credentials: false, origin: 'http://multimedia--chat-api.herokuapp.com/*'}));
+        next();
+    });
     const server = http.createServer(app);
     configSocket(server);
     app.use(logger("dev"));
     app.use(express.json());
 	app.use(express.urlencoded({ extended: true }));
-	
+	app.use(cors("*"));
     app.use(methodOverride("X-HTTP-Method-Override"));
     app.use(methodOverride((req) => {
         if (req.body && typeof req.body === "object" && "_method" in req.body) {
@@ -35,6 +34,7 @@ export default (app) => {
         return undefined;
     }));
     connectDB();
+    
     app.use("/api/v1", initRoutes);
     app.use("*", (req, res) => res.status(404).json({
 					status: 404,
