c9c62638d17f4f0c4b94e5486e3f747343254ac1
lvtrai <52622764+lvtrai@users.noreply.github.com>
2020-11-25 16:31:05 +0700

    change config socket

--- src/config/socket.js
+++ src/config/socket.js
@@ -7,7 +7,17 @@ const users = {};
 const socketToRoom = {};
 
 export default function (server) {
-    const io = socket(server)
+    const io = socket(server,{
+        handlePreflightRequest: (req, res) => {
+            const headers = {
+                "Access-Control-Allow-Headers": "Content-Type, Authorization",
+                "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+                "Access-Control-Allow-Credentials": true
+            };
+            res.writeHead(200, headers);
+            res.end();
+        }
+    })
 
     ConversationService.io = io.of('/Conversation').on('connection', (client) => {
         client.on('join', (id) => {
