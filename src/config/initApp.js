b74eb5c8bf732eada1feacdcf7214796b9f28ffa
nguyenthitram2810 <nguyenthitram2010a@gmail.com>
2020-11-25 21:28:14 +0700

    fix bug

--- src/config/initApp.js
+++ src/config/initApp.js
@@ -10,16 +10,11 @@ import configSocket from '../config/socket';
 
 export default (app) => {
     app.use((req, res, next) => {
-    //   const allowedOrigins = ['*'];
-    //   const origin = req.headers.origin;
-    //   if (allowedOrigins.includes(origin)) {
-    //        res.setHeader('Access-Control-Allow-Origin', origin);
-    //   }
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      res.header('Access-Control-Allow-Credentials', true);
-      return next();
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+        next();
     });
     const server = http.createServer(app);
     
