feca20c60540a6d5d9dd86caab76fcefc40b3b97
nguyenthitram2810 <nguyenthitram2010a@gmail.com>
2020-11-25 21:17:02 +0700

    config

--- src/config/initApp.js
+++ src/config/initApp.js
@@ -9,29 +9,34 @@ import Model from '../database/models/index';
 import configSocket from '../config/socket';
 
 export default (app) => {
+    app.use((req, res, next) => {
+      const allowedOrigins = ['*'];
+      const origin = req.headers.origin;
+      if (allowedOrigins.includes(origin)) {
+           res.setHeader('Access-Control-Allow-Origin', origin);
+      }
+
+      res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      res.header('Access-Control-Allow-Credentials', true);
+      return next();
+    });
     const server = http.createServer(app);
-    configSocket(server);
     app.use(logger("dev"));
     app.use(express.json());
 	app.use(express.urlencoded({ extended: true }));
-    app.use((res, next) => {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-        next();
-    })
     app.use(methodOverride("X-HTTP-Method-Override"));
     app.use(methodOverride((req) => {
         if (req.body && typeof req.body === "object" && "_method" in req.body) {
             const method = req.body._method;
             delete req.body._method;
-
             return method;
         }
 
         return undefined;
     }));
     connectDB();
+    configSocket(server);
     app.use("/api/v1", initRoutes);
     app.use("*", (req, res) => res.status(404).json({
 					status: 404,
